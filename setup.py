0b36a3f587099065e9ba1838046befff00a8fec8
Michael Graber <michigraber@gmail.com>
2014-05-13 16:33:28 +0000

    fix in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='desdmdashboard',
       version='0.1.0',
       description=('Web App for timeseries of snapshot data monitoring for '
-                    'DESDM. \nRemote components only!',
+                    'DESDM. \nRemote components only!'),
       author='Michael Graber',
       author_email='michael.graber@fhnw.ch',
       packages=[
