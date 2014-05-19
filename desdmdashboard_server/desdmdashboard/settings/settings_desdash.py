a59cc80b8b5bf097c874f5b7672303b4b946cf98
Michael Graber <michigraber@gmail.com>
2014-05-19 09:25:34 +0000

    desdmdashboard deployment settings separated.

---
+++ desdmdashboard_server/desdmdashboard/settings/settings_desdash.py
@@ -0,0 +1,5 @@
+
+# settings for the deployment of the desdmdashboard on 
+# desdash.cosmology.illinois.edu 
+
+from common_settings import *
--- desdmdashboard_server/desdmdashboard/wsgi.py
+++ desdmdashboard_server/desdmdashboard/wsgi.py
@@ -6,9 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+import sys
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "desdmdashboard.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "desdmdashboard.settings.settings_desdash")
+
+sys.path.append('/webapps/dev/desdmdashboard_server') #parent directory of project
+sys.path.append('/webapps/dev/desdmdashboard_server/desdmdashboard')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
