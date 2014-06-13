4305bf1f5f4c90dcec2842dcfb394c3384480a96
Michael Graber <michigraber@gmail.com>
2014-06-13 16:13:45 +0000

    import data fix

--- data_import/import_deshist.py
+++ data_import/import_deshist.py
@@ -10,5 +10,11 @@ def send_to_desdmdashboard(deshist):
 
     for i in range(len(deshist)):
         jrow = deshist.irow(i).to_json()
-        send_metric_value('deshist', jrow, value_type='json')
+        request = send_metric_value('deshist', jrow, value_type='json')
+        if request.error_status[0]:
+            print request.error_status[1]
 
+
+if __name__ == '__main__':
+
+    send_to_desdmdashboard(deshist)
