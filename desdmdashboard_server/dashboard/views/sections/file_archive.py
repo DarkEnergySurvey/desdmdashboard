2edb97f5d371375dff321259715cdff9b922563c
Michael Graber <michigraber@gmail.com>
2014-06-16 15:12:44 +0000

    tiny tiny thing.

--- desdmdashboard_server/dashboard/views/sections/file_archive.py
+++ desdmdashboard_server/dashboard/views/sections/file_archive.py
@@ -9,7 +9,7 @@ def archive_sizes():
             ('size archive_home_mjohns44', 'mgraber',),
             ('size archive_target_mjohns44', 'mgraber',),
             ('size desdev1_mmg', 'mgraber',),),
-            resample='20Min',
+            resample='D',
             )
     figstring = plot_df_to_svg_string(df, style='.-')
     return figstring
