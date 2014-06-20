ba43dea4d34aeca2578c61ade1f537dfb742e97a
Michael Graber <michigraber@gmail.com>
2014-06-20 18:14:04 +0000

    file archive dashboard section updated

--- desdmdashboard_server/dashboard/views/sections/file_archive.py
+++ desdmdashboard_server/dashboard/views/sections/file_archive.py
@@ -5,10 +5,7 @@ def size():
     '''
     '''
     df = pandas_utils.get_multimetric_dataframe(
-            (('size desar2home', 'mgraber',),
-            ('size archive_home_mjohns44', 'mgraber',),
-            ('size archive_target_mjohns44', 'mgraber',),
-            ('size desdev1_mmg', 'mgraber',),),
+            (('size desar2home', 'mgraber',),),
             resample='D',
             )
     figstring = plot_df_to_svg_string(df, style='.-', logy=True)
