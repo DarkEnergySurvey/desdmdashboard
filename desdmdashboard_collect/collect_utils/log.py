9c42c48d15211b8c8e615f468055c01fb53e6fdb
Michael Graber <michigraber@gmail.com>
2014-07-09 23:00:10 +0000

    collect cronjob configuration

--- desdmdashboard_collect/collect_utils/log.py
+++ desdmdashboard_collect/collect_utils/log.py
@@ -4,8 +4,10 @@
 import os
 import logging
 
-loggers = {}
+LOG_DIR = os.environ['COLLECT_LOG_DIR']
+LOG_FILE = os.environ['COLLECT_LOG_FILE']
 
+loggers = {}
 
 def get_logger(name):
     global loggers
@@ -14,20 +16,21 @@ def get_logger(name):
         return loggers.get(name)
 
     else:
-	logging.basicConfig(level=logging.DEBUG)
-	logger = logging.getLogger(name)
+        logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger(name)
+        desdash_logfile_path = os.path.normpath(LOG_DIR)
+        if not os.path.exists(desdash_logfile_path):
+            os.makedirs(desdash_logfile_path)
+        logfile = os.path.join(desdash_logfile_path, LOG_FILE)
 
-        logfile = 'desdmdashboard_collect.log'
-	desdash_logfile_path = os.path.normpath('/desdmdashboard_collect/log/')
-        if os.path.exists(desdash_logfile_path):
-	    logfile = os.path.join(desdash_logfile_path, logfile)
-	handler = logging.FileHandler(logfile)
-	handler.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(logfile)
+        handler.setLevel(logging.DEBUG)
 
-	formatter = logging.Formatter("[ %(asctime)s ] [ %(levelname)s ] [ %(module)s ] : %(message)s",
-	    		      "%Y-%m-%d %H:%M:%S")
-	handler.setFormatter(formatter)
-	logger.addHandler(handler)
+        formatter = logging.Formatter(
+                "[ %(asctime)s ] [ %(levelname)s ] [ %(module)s ] : %(message)s",
+	    		"%Y-%m-%d %H:%M:%S")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
         loggers[name] = logger
 
