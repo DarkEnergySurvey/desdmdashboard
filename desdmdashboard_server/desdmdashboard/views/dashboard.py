351d11c91e6591b6e56a5b840fdf68c69721b364
Michael Graber <michigraber@gmail.com>
2014-07-15 03:30:49 +0000

    unused function removed

--- desdmdashboard_server/desdmdashboard/views/dashboard.py
+++ desdmdashboard_server/desdmdashboard/views/dashboard.py
@@ -4,8 +4,6 @@ from docutils.core import publish_parts
 
 from django.shortcuts import render, render_to_response, get_object_or_404
 
-from . import dashboard_figures
-
 
 def docu(request):
     '''
