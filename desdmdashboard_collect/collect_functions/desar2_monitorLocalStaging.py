bccfec7251e239e6def3c3b170c23b6d8469296f
Michael Graber <michigraber@gmail.com>
2014-07-24 12:33:51 +0000

    desar2 local staging jobs added, functions tuned

--- desdmdashboard_collect/collect_functions/desar2_monitorLocalStaging.py
+++ desdmdashboard_collect/collect_functions/desar2_monitorLocalStaging.py
@@ -6,10 +6,9 @@ DESDMDASHBOARD DATA COLLECTION FUNCTION example : monitor local staging area dis
 
 from desdmdashboard_remote.senddata import decorators 
 
-from collect_utils.commandline import shell_command
-from collect_utils.commandline import du_dir_Mb
+from desdmdashboard_collect.collect_utils.commandline import du_dir_Mb
 
-from collect_utils import log 
+from desdmdashboard_collect.collect_utils import log 
 
 logger = log.get_logger('desdmdashboard_collect')
 
@@ -22,18 +21,16 @@ def monitor_local_staging_via_du(path):
     '''
     '''
     logger.info('monitor_local_staging_via_du entered.')
-    logger.info('executing du')
+    logger.info('executing du on '+path)
 
     try:
         du_return = du_dir_Mb(path=path)
         logger.info('du_dir_Mb successfully executed.')
     except Exception, err:
         logger.error('du_dir_Mb failed: '+err)
-	return
 
     return du_return
 
 
 if __name__ == '__main__':
-    monitor_local_staging_via_du()
-
+    monitor_local_staging_via_du('~/')
