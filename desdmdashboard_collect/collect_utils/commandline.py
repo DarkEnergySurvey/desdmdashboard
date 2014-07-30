b2b1b2c70c56c7659f0f552be15d3fadaa2329f5
Michael Graber <michigraber@gmail.com>
2014-07-30 13:00:14 +0000

    logging for command line functions + cleanup.

--- desdmdashboard_collect/collect_utils/commandline.py
+++ desdmdashboard_collect/collect_utils/commandline.py
@@ -5,20 +5,34 @@ DESDMDashboard data collection utility database functions
 
 import subprocess
 
+try:
+    from desdmdashboard_collect.collect_utils import log
+    logger = log.get_logger('desdmdashboard_collect')
+except:
+    logger = None
+
+
+class DataCollectionCommandLineError(Exception):
+    pass
+
 
 def shell_command(cmd):
+    if logger:
+        logger.info('Executing shell command through subprocess module.')
+        logger.debug('command: ' + ' '.join(cmd))
     p = subprocess.Popen(cmd, shell=True,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
-    return out.rsplit('\n'), err.rsplit('\n')
+    if logger and err:
+        logger.error(err)
+    elif err:
+        raise DataCollectionCommandLineError(err)
+    return out, err
 
 
 def du_dir_Mb(path):
     out, err = shell_command('du -s -m ' + path)
-    if any(err):
-        raise CommandLineError(err)
+    if err:
+        raise DataCollectionCommandLineError(err)
+    out = out.rsplit('\n')
     return out[0].rsplit('\t')[0]
-
-
-class CommandLineError(Exception):
-    pass
