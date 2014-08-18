f4e1c023fe13325fee8fd01be16ee860b87a1e25
Michael Graber <michigraber@gmail.com>
2014-08-18 12:03:37 +0000

    docstring to test collect function added.

--- desdmdashboard_collect/collect_functions/test_functions.py
+++ desdmdashboard_collect/collect_functions/test_functions.py
@@ -10,12 +10,25 @@ from desdmdashboard_remote.senddata import decorators
 from desdmdashboard_collect.collect_utils import log 
 logg = log.get_logger('desdmdashboard_collect')
 
-@decorators.Monitor('test', value_type='char', logger=logg)
+@decorators.Monitor('test', value_type='int', logger=logg)
 def test_collect_function():
     '''
+    My test docu
+    <<<<<<<<<<<<
+
+    section 1
+    ~~~~~~~~~
+    Now we even have a section
+
+    - and
+    - believe it
+    - or not
+
+    a **list** and *bold* and *italic* print.
+
     '''
     logg.info('in test_collect_function')
-    return 'test output'
+    return 10
 
 if __name__ == '__main__':
     test_collect_function()
