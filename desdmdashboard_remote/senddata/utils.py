a25bc1f1e478b1390f2bb01121a5fe7d971d8008
Michael Graber <michigraber@gmail.com>
2014-08-26 21:10:50 +0000

    senddata utils added ..

---
+++ desdmdashboard_remote/senddata/utils.py
@@ -0,0 +1,17 @@
+
+
+'''
+The values that can be sent to the webapp.
+
+'''
+
+DATA_TEMPLATE = {
+    'name': u'',
+    'has_error': False, 
+    'error_message': u'',
+    'value_type_': u'',
+    'value': u'',
+    'tags': u'',
+    'time_': u'',
+    'doc': u'',
+    }
