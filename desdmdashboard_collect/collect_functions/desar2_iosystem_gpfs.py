c61abacb5f5e2587ed1cdc37ad0339b03b1d7530
daues <daues>
2014-08-26 19:38:33 +0000

    functions for connections metrics, did not commit earlier

---
+++ desdmdashboard_collect/collect_functions/desar2_iosystem_gpfs.py
@@ -0,0 +1,38 @@
+"""
+
+Acquire Relevant metrics at the sytems level
+
+"""
+import subprocess
+import sys 
+
+from desdmdashboard_remote.senddata import decorators
+
+from desdmdashboard_remote.senddata.decorators import Monitor
+from desdmdashboard_collect.collect_utils import log
+
+logger = log.get_logger('desdmdashboard_collect')
+
+def wc_l(cmd) :
+    import subprocess
+    """ Return the number of lines on stdout for the command   """
+    stdout, stderr = subprocess.Popen(cmd, shell="True", stdout=subprocess.PIPE).communicate()
+    if stderr:
+        logger.warning('archive to fnal error: %s ' %(stderr))
+        raise commandline.DataCollectionCommandLineError(stderr)
+    return int(stdout)
+
+@decorators.Monitor('desar_conn_to_gpfs', value_type='int', logger=logger)
+def gpfs_connections() :
+    cmd = "netstat -t | grep gpfs | grep ESTABLISHED |  wc -l"
+    return wc_l(cmd)
+    """ Return number of connections to the STKEN systems at fermilab 
+        Connections are normaly open when sending data to the disaster                                                                                             
+        recovery store at FNAL.                                                                                                  
+    """
+    cmd = "netstat -t | grep stken.*fnal | grep ESTABLISHED |  wc -l"
+    return wc_l(cmd)
+
+
+
+
---
+++ desdmdashboard_collect/collect_functions/desar2_iosystem_noao.py
@@ -0,0 +1,35 @@
+"""
+
+Acquire Relevant metrics at the sytems level
+
+"""
+import subprocess
+import sys 
+
+from desdmdashboard_remote.senddata import decorators
+
+from desdmdashboard_remote.senddata.decorators import Monitor
+from desdmdashboard_collect.collect_utils import log
+
+logger = log.get_logger('desdmdashboard_collect')
+
+def wc_l(cmd) :
+    import subprocess
+    """ Return the number of lines on stdout for the command   """
+    stdout, stderr = subprocess.Popen(cmd, shell="True", stdout=subprocess.PIPE).communicate()
+    if stderr:
+        logger.warning('archive to fnal error: %s ' %(stderr))
+        raise commandline.DataCollectionCommandLineError(stderr)
+    return int(stdout)
+
+@decorators.Monitor('desar_conn_to_noao', value_type='int', logger=logger)
+def noao_connections() :
+    cmd = "netstat -t | grep noao | grep ESTABLISHED |  wc -l"
+    return wc_l(cmd)
+    """ Return number of connections to the GPFS systems at NCSA.                                                                 
+    """
+    cmd = "netstat -t | grep stken.*fnal | grep ESTABLISHED |  wc -l"
+    return wc_l(cmd)
+
+
+
