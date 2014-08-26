4df95ac2f8b7fbee8f615888dc719863468c178c
Michael Graber <michigraber@gmail.com>
2014-08-26 21:07:15 +0000

    send data functions generalized

--- desdmdashboard_remote/senddata/functions.py
+++ desdmdashboard_remote/senddata/functions.py
@@ -5,24 +5,6 @@ from utils import DATA_TEMPLATE
 from ..http_requests import Request
 
 
-def send_metric_value(metric_name, value, **kwargs):
-    '''
-    Function sends value to metric on the DESDMDASHBOARD.
-
-    Returns a request object.
-    '''
-    data = DATA_TEMPLATE.copy()
-
-    data['name'] = metric_name
-    data['value'] = value 
-    data.update(kwargs)
-
-    request = Request()
-    request.POST(data=data)
-
-    return request
-
-
 def send_metric_data(**kwargs):
     '''
     Send any of the possible arguments to the DESDMDASHBOARD db as kwargs of
@@ -30,10 +12,36 @@ def send_metric_data(**kwargs):
 
     Returns a request object.
     '''
+
+    logger = kwargs.pop('logger', None)
+
     data = DATA_TEMPLATE.copy()
     data.update(kwargs)
+
+    if self.logger:
+        mess = 'Sending value {val} to metric {met}'
+        self.logger.info(mess.format(
+            val=data['value'], met=data['name'])
+            )
 
     request = Request()
     request.POST(data=data)
 
+    if request.error_status[0]:
+        logger.error('sending data failed: ' + request.error_status[1])
+    else:
+        logger.info('data successfully sent.')
+
     return request
+
+
+def send_metric_value(metric_name, value, **kwargs):
+    '''
+    Function sends value to metric on the DESDMDASHBOARD.
+
+    Returns a request object.
+    '''
+    request = send_metric_data(name=metric, value=value, **kwargs)
+    return request
+
+
