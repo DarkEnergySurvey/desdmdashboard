14470c9332ebda50c8f0fbe2a787fe5c9421a33a
daues <daues>
2014-08-26 20:13:08 +0000

    jobs for the connections metrics

---
+++ desdmdashboard_collect/collect_jobs/desar2_1_hourly_fconn.py
@@ -0,0 +1,21 @@
+
+
+from desdmdashboard_collect.collect_utils import log
+logger = log.get_logger('desdmdashboard_collect')
+
+
+# from desdmdashboard_collect.collect_functions.system_metrics import sick_httpd
+# from desdmdashboard_collect.collect_functions.desar2_iosystem import stken_connections
+from desdmdashboard_collect.collect_functions.desar2_iosystem_fconn import fermigrid_connections
+# from desdmdashboard_collect.collect_functions.desar2_iosystem import noao_connections
+# from desdmdashboard_collect.collect_functions.desar2_iosystem import gpfs_connections
+# from desdmdashboard_collect.collect_functions.desar2_iosystem import any_connections
+
+def main():
+    fermigrid_connections()
+    
+
+if __name__ == '__main__':
+    logger.info('Start 1 hourly data collection script. (fermigrid_connections)')
+    main()
+    logger.info('1 hourly data collection script finished. (fermigrid_connections)')
---
+++ desdmdashboard_collect/collect_jobs/desar2_1_hourly_gpfs.py
@@ -0,0 +1,21 @@
+
+
+from desdmdashboard_collect.collect_utils import log
+logger = log.get_logger('desdmdashboard_collect')
+
+
+# from desdmdashboard_collect.collect_functions.system_metrics import sick_httpd
+# from desdmdashboard_collect.collect_functions.desar2_iosystem_stken import stken_connections
+# from desdmdashboard_collect.collect_functions.desar2_iosystem_fconn import fermigrid_connections
+# from desdmdashboard_collect.collect_functions.desar2_iosystem import noao_connections
+from desdmdashboard_collect.collect_functions.desar2_iosystem_gpfs import gpfs_connections
+# from desdmdashboard_collect.collect_functions.desar2_iosystem import any_connections
+
+def main():
+    gpfs_connections()
+    
+
+if __name__ == '__main__':
+    logger.info('Start 1 hourly data collection script. (gpfs_connections)')
+    main()
+    logger.info('1 hourly data collection script finished. (gpfs_connections)')
