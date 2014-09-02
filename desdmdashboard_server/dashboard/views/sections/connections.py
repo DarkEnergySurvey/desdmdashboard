e1c8b4bf741454abe44da9696c67a38d9e85df08
databot <databot>
2014-09-02 14:02:22 +0000

    add a 2 Day plot alongside 7 Day plot

--- desdmdashboard_server/dashboard/views/sections/connections.py
+++ desdmdashboard_server/dashboard/views/sections/connections.py
@@ -1,6 +1,31 @@
 from monitor import pandas_utils
 from dashboard.views.plotutils import plot_df_to_svg_string
 
+def connections2D_summary():
+
+    df, metrics = pandas_utils.get_multimetric_dataframe(
+            (('gdaues', 'desar_conn_to_any', ),
+                ('gdaues', 'connections_to_fermigrid', ),
+                ('gdaues', 'desar_conn_to_noao', ),
+                ('gdaues', 'desar_conn_to_gpfs', ),
+                ('gdaues', 'connections_to_stken', ),),
+            resample='10Min',
+            )
+    # do anything with panda you want
+
+    #get serillizes plot
+    figstring = plot_df_to_svg_string(df.last('2D'), 
+            metrics=metrics,
+            style='.-', y_label='# Connections',
+            ylim=(0, 100), colormap='spectral')
+
+    sectiondict = {
+            'title': 'Network connections 2 Days',
+            'content_html': figstring, 
+            }
+
+    return sectiondict
+
 def connections_summary():
     df, metrics = pandas_utils.get_multimetric_dataframe(
             (('gdaues', 'desar_conn_to_any', ),
@@ -15,11 +40,11 @@ def connections_summary():
     #get serillizes plot
     figstring = plot_df_to_svg_string(df.last('7D'), 
             metrics=metrics,
-            style='.-', y_label='# Connections',
+            style='-', y_label='# Connections',
             ylim=(0, 100), colormap='spectral')
 
     sectiondict = {
-            'title': 'Network connections',
+            'title': 'Network connections 7 Days',
             'content_html': figstring, 
             }
 
